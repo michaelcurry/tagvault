7d2c2e344e84be565358c8ee8a20c654d0795b3f
michaelcurry <michael.ian.curry@gmail.com>
2013-08-19 15:42:48 -0700

    Slideshow changed

--- index.php
+++ index.php
@@ -6,29 +6,20 @@
   }elseif ($_REQUEST && isset($_REQUEST['event']) && isset($_REQUEST['slide']) && file_exists('bin/'.$_REQUEST['event'].'.csv')) { ?>
 <html>
   <head>
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
     <link href="./style2.css" rel="stylesheet">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="cycle.js"></script>
   </head>
   <body>
-    <div class="container" >
-      <div class="row-fluid">
-        <div class="span12">
-        <div id="slideshow" >
-          <?php $mms = str_getcsv(file_get_contents('bin/'.strtolower($_REQUEST['event']).'.csv'),'|');?>
-          <?php foreach (array_reverse($mms) as $message) : ?>
-            <?php if ($message) : ?>
-              <?php $m = json_decode($message); ?>
-              <img src="<?php echo $m->img ?>" />
-            <?php endif; ?>
-          <?php endforeach; ?>
-        </div>
-        </div>
-      </div>
+    <div id="slideshow" >
+      <?php $mms = str_getcsv(file_get_contents('bin/'.strtolower($_REQUEST['event']).'.csv'),'|');?>
+      <?php foreach (array_reverse($mms) as $message) : ?>
+        <?php if ($message) : ?>
+          <?php $m = json_decode($message); ?>
+          <img src="<?php echo $m->img ?>" />
+        <?php endif; ?>
+      <?php endforeach; ?>
     </div>
-
     <script type="text/javascript" charset="utf-8">
       $(document).ready(function() {
           $('#slideshow').cycle({
