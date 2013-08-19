3f9ff03b2860b084b7fa4bcf042525f5ceabb648
michaelcurry <michael.ian.curry@gmail.com>
2013-08-19 15:26:53 -0700

    slide added

--- index.php
+++ index.php
@@ -3,6 +3,44 @@
     $file = 'img/'.sha1(md5($_REQUEST['MediaUrls'])).'.jpg';
     file_put_contents($file, file_get_contents($_REQUEST['MediaUrls']));
     file_put_contents('bin/'.strtolower($_REQUEST['Body']).'.csv', '{"img":"'.$file.'","place":"'.$_REQUEST['FromCity'].', '.$_REQUEST['FromState'].'","time":"'.date('F jS Y h:i:s A').'"}|', FILE_APPEND);
+  }elseif ($_REQUEST && isset($_REQUEST['event']) && isset($_REQUEST['slide']) && file_exists('bin/'.$_REQUEST['event'].'.csv')) { ?>
+<html>
+  <head>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link href="./style2.css" rel="stylesheet">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="cycle.js"></script>
+  </head>
+  <body>
+    <div class="container" >
+      <div class="row-fluid">
+        <div class="span12">
+        <div id="slideshow" >
+          <?php $mms = str_getcsv(file_get_contents('bin/'.strtolower($_REQUEST['event']).'.csv'),'|');?>
+          <?php foreach (array_reverse($mms) as $message) : ?>
+            <?php if ($message) : ?>
+              <?php $m = json_decode($message); ?>
+                  <img src="<?php echo $m->img ?>" />
+              </div>
+            <?php endif; ?>
+          <?php endforeach; ?>
+        </div>
+        </div>
+      </div>
+    </div>
+
+    <script type="text/javascript" charset="utf-8">
+      $(document).ready(function() {
+          $('#slideshow').cycle({
+          fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+        });
+      });
+    </script>
+  </body>
+</html>
+
+<?php
   }elseif ($_REQUEST && isset($_REQUEST['event']) && file_exists('bin/'.$_REQUEST['event'].'.csv')) { ?>
 <html>
   <head>
