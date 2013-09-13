8dcf567fc2aa729335a69159d3e6d91b84d8f1fa
michaelcurry <michael.ian.curry@gmail.com>
2013-09-13 11:13:39 -0700

    composer init

--- index.php
+++ index.php
@@ -1,19 +1,27 @@
 <?php
+	/**
+	 * Routing file
+	 */
 
-	// If request is a POST form Twilio (MMS)
-	if ($_REQUEST && isset($_REQUEST['Body']) && isset($_REQUEST['MediaContentTypes']) && strpos($_REQUEST['MediaContentTypes'], 'image') !== false )
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') // If request is a POST
 	{
 		include_once('post.php');
-	} // If request is for a tag && requests a slide-show
-	elseif ($_REQUEST && isset($_REQUEST['tag']) && isset($_REQUEST['slide']) && file_exists('bin/'.$_REQUEST['tag'].'.csv'))
+	}
+	else // If request is a GET
 	{
-		include_once('slide.php');
-	} // If request is for a tag
-	elseif ($_REQUEST && isset($_REQUEST['tag']) && file_exists('bin/'.$_REQUEST['tag'].'.csv'))
-	{
-		include_once('gallery.php');
-	}// Reques Fall back ( We don't want random people to get in! )
-	else
-	{
-		include_once('fallback.php');
+		if ( isset($_GET['tag']) ) // If tag is requested
+		{
+			if ( isset($_REQUEST['slide']) ) // If slide-show is requested
+			{
+				include_once('slide.php');
+			}
+			else // Gallery
+			{
+				include_once('gallery.php');
+			}
+		}
+		else // Fallback
+		{
+			include_once('fallback.php');
+		}
 	}
