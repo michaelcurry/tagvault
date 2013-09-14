eef11b53bcb14e996bd62891180948a1e8aaebc2
michaelcurry <michael.ian.curry@gmail.com>
2013-09-13 17:11:00 -0700

    array_unique on list

--- gallery.php
+++ gallery.php
@@ -15,7 +15,7 @@
 					<h1>Tag: <i><?php echo $_GET['tag']; ?></i></h1>
 					<div id="mason" class="js-masonry">
 					<?php $redis = new Predis\Client(); ?>
-					<?php foreach($redis->lrange(strtolower(trim($_GET['tag'])), 0, -1)  as $entry) : ?>
+					<?php foreach(array_unique($redis->lrange(strtolower(trim($_GET['tag'])), 0, -1))  as $entry) : ?>
 						<?php $entry = $redis->get($entry); ?>
 						<?php $entry = json_decode($entry); ?>
 						<div class="image">
