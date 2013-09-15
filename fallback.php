c535498c9df24afccef8de121f7c488d41969495
michaelcurry <michael.ian.curry@gmail.com>
2013-09-15 11:27:35 -0700

    add twitter

--- fallback.php
+++ fallback.php
@@ -8,9 +8,17 @@
 		<script src="./inc/js/masonry.pkgd.min.js"></script>
 	</head>
 	<body>
-			<a href="https://github.com/michaelcurry/tagvault"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>		<div class="container text-center">
-			<img class="logo img-responsive" src="./inc/img/logo.png" />
+		<div class="credits">
 
+			<a href="https://twitter.com/intent/tweet?screen_name=KernelCurry" class="twitter-mention-button">Tweet author @KernelCurry</a>
+			<br />
+			<a href="https://twitter.com/share" class="twitter-share-button" data-text="Check out this cool MMS BBS @KernelCurry created!" data-hashtags="TagVault">Tweet</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+		</div>
+		<a href="https://github.com/michaelcurry/tagvault"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
+		<div class="container text-center">
+			<img class="logo img-responsive" src="./inc/img/logo.png" />
+			<p class="intro" >Text <span>26704</span> a picture with the name of a tag.  Your image will be displayed on that tag.</p>
 		</div>
 	</body>
 </html>
