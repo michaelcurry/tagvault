19e9e6448d1abe7c53d3aa1b1f53ebc8055a3649
michaelcurry <michael.ian.curry@gmail.com>
2013-09-15 11:05:12 -0700

    fallback.php style init

--- fallback.php
+++ fallback.php
@@ -2,12 +2,15 @@
 <html>
 	<head>
 		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-		<link href="./inc/style.css" rel="stylesheet">
+		<link href="./inc/css/style.css" rel="stylesheet">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-		<script src="./inc/masonry.pkgd.min.js"></script>
+		<script src="./inc/js/masonry.pkgd.min.js"></script>
 	</head>
 	<body>
+			<a href="https://github.com/michaelcurry/tagvault"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>		<div class="container text-center">
+			<img class="logo img-responsive" src="./inc/img/logo.png" />
 
+		</div>
 	</body>
 </html>
