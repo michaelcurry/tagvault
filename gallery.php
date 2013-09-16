4edfd123ae992bf01b4bae0fa6e401726390e526
michaelcurry <michael.ian.curry@gmail.com>
2013-09-15 17:22:56 -0700

    more .image changes

--- gallery.php
+++ gallery.php
@@ -42,8 +42,7 @@
 							<a href="/img_processed/<?php echo $entry->file ?>" title="<?php echo $entry->datetime ?>" >
 								<img class="img-responsive" src="/img_processed/<?php echo $entry->file ?>" />
 							</a>
-							<p class="place">Country: <?php echo $entry->country?></p>
-							<p class="time clearfix"><?php echo $entry->datetime?></p>
+							<p class="info">Country: <?php echo $entry->country?> | <?php echo $entry->datetime?></p>
 						</div>
 					<?php endforeach; ?>
 					</div>
