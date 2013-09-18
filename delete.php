81d3ddf3fbaab442edc0118186e67e709c28ada9
michaelcurry <michael.ian.curry@gmail.com>
2013-09-17 18:49:10 -0700

    delete.php path fix "

--- delete.php
+++ delete.php
@@ -6,7 +6,7 @@
 	}
 
 	// Rename File
-	if ( file_exists('/img_processed/.'.$_GET['del']) )
+	if ( file_exists('/img_processed/'.$_GET['del']) )
 	{
 		move_uploaded_file('/img_processed/.'.$_GET['del'], '/img_deleted/.'.$_GET['del']);
 	}
